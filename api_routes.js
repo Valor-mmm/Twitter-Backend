01c82ff5f0a79d6998ce078500f0068277ae15de
Valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2017-12-25 12:42:43 +0100

    Adding user api. Not finished yet.

--- api_routes.js
+++ api_routes.js
@@ -1,10 +1,41 @@
+'use strict';
+
+const _ = require('lodash');
+
+const userApi = require('./app/api/user-api');
+
+const apiPath = '/api/';
+const userPath = 'users';
+
 const corsOption = {
   cors: true
 };
 
+const securityOption = {
+
+};
+
+const getOptions = (specificOptions) => {
+  const secureCorsOption = _.merge(corsOption, securityOption);
+  return _.merge(secureCorsOption, specificOptions);
+};
+
+const getPath = (path, id) => {
+  const basePath = apiPath + path;
+  if (id) {
+    return `${basePath}/{id}`;
+  }
+  return basePath;
+};
+
 const apiRoutes = [
 
-  //{method: , path: , handler: , options: corsOption}
+  {method: 'POST', path: getPath(userPath), options: getOptions(userApi.create)},
+  {method: 'GET', path: getPath(userPath, true), options: getOptions(userApi.getOne)},
+  {method: 'GET', path: getPath(userPath), options: getOptions(userApi.getSomeById)},
+  {method: 'PUT', path: getPath(userPath, true), options: getOptions(userApi.update)},
+  {method: 'DELETE', path: getPath(userPath, true), options: getOptions(userApi.deleteOne)},
+  {method: 'DELETE', path: getPath(userPath), options: getOptions(userApi.deleteSomeById)},
 
 ];
 
---
+++ app/api/user-api.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const Joi = require('joi');
+
+const User = require('../models/user/user');
+const apiUtils = require('./api-utils');
+const validationUtils = require('./validation-utils');
+
+const modelName = 'User';
+
+const getUserProperties = function (required) {
+  return {
+    username: Joi.string().required(required),
+    email: Joi.string().required(required),
+    password: Joi.string().required(required),
+    firstName: Joi.string().required(required),
+    lastName: Joi.string().required(required),
+    tweets: Joi.array().items(Joi.string()),
+    following: Joi.array().items(Joi.string()),
+    _id: validationUtils.idSchema()
+  };
+};
+
+const create = {
+  auth: false,
+
+  validate: {
+    payload: getUserProperties(true),
+
+    failAction: validationUtils.validationErrHandler
+  },
+
+  handler: (request, h) => {
+    return apiUtils.create(modelName, request.payload, User);
+  }
+};
+
+const getOne = {
+  auth: false,
+
+  validate: validationUtils.getIdParamsValidation(),
+
+  handler: (request, h) => {
+    return apiUtils.findById(modelName, User, request.params.id);
+  }
+};
+
+const getSomeById = {
+  auth: false,
+
+  validate: validationUtils.getIdArrayValidation(),
+
+  handler: (request, h) => {
+    const constraints = {
+      _id: {$in: request.payload.ids}
+    };
+
+    return apiUtils.find(modelName, User, constraints);
+  }
+};
+
+const update = {
+  auth: false,
+
+  validate: {
+
+    payload: getUserProperties(false),
+
+    params: {
+      id: validationUtils.idSchema()
+    },
+
+    failAction: validationUtils.validationErrHandler
+  },
+
+  handler: (request, h) => {
+    return apiUtils.update(modelName, User, request.params.id, request.payload);
+  }
+};
+
+const deleteOne = {
+  auth: false,
+
+  validate: validationUtils.getIdParamsValidation(),
+
+  handler: (request, h) => {
+    const constraints = {
+      _id: request.params.id
+    };
+    return apiUtils.delete(modelName, User, constraints);
+  }
+};
+
+const deleteSomeById = {
+  auth: false,
+
+  validate: validationUtils.getIdArrayValidation(),
+
+  handler: (request, h) => {
+    const constraints = {
+      id: {$in: request.payload.ids}
+    };
+    return apiUtils.delete(modelName, User, constraints);
+  }
+};
+
+exports.create = create;
+exports.getOne = getOne;
+exports.getSomeById = getSomeById;
+exports.update = update;
+exports.deleteOne = deleteOne;
+exports.deleteSomeById = deleteSomeById;
