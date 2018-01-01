397c8936ecae8919da93b54ed6929bbb65a04263
Valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2018-01-01 17:23:27 +0100

    Adding default auth strategy jwt from hapi-now-auth

--- index.js
+++ index.js
@@ -3,17 +3,32 @@
 const Hapi = require('hapi');
 const logger = require('simple-node-logger').createSimpleLogger();
 
+const authUtils = require('./app/api/auth-utils');
+
 const options = {
   port: process.env.PORT || 4000
 };
 
 const server = Hapi.server(options);
 
+const jwtConfig = authUtils.getJwtConfig();
+const authJwtOptions = {
+  verifyJWT: true,
+  keychain: [jwtConfig.password],
+  validate: authUtils.validate,
+  verifyOptions: {algorithms: [jwtConfig.algorithm]}
+};
+
 async function startServer() {
 
   try {
-    //await server.register();
+    const jwtPlugin = require('@now-ims/hapi-now-auth');
+    await server.register({
+      plugin: jwtPlugin
+    });
     require('./app/models/db');
+    server.auth.strategy('jwt-strategy', 'hapi-now-auth', authJwtOptions);
+    server.auth.default('jwt-strategy');
     await server.start();
     await server.route(require('./api_routes'));
     logger.info(`Server successfully started at port: ${options.port}`);
